3322e660ab97f0f0f3d4875d04b2736d3aa0ea9b
ncaudill27 <nelson.a.caudill@gmail.com>
2019-11-29 08:57:34 -0500

    Expand cli.rb to prompt game path

--- lib/hollypop/cli.rb
+++ lib/hollypop/cli.rb
@@ -2,5 +2,18 @@ class HollyPop::Cli
 
     def call
         puts "Welcome to HollyPop!"
+        start_game
+    end
+    
+    def start_game
+        puts "Which artist would you like to choose?"
+        # call #list_artists or Artists.all
+        artists = ["Hugh Jackman", "Ryan Reynolds"]
+        #input = gets.chomp
+        generate_question
+    end
+
+    def generate_question
+        puts "Question related to chosen Artist"
     end
 end
